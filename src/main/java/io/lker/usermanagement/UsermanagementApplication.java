bf6222f6442af666d863d390cbe5fc3a298fbf06
Ryan <ryantwalk@gmail.com>
2019-01-06 16:38:14 -0500

    added logging w/ slf4j. Closes #14.

--- src/main/java/io/lker/usermanagement/UsermanagementApplication.java
+++ src/main/java/io/lker/usermanagement/UsermanagementApplication.java
@@ -1,13 +1,16 @@
 package io.lker.usermanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class UsermanagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsermanagementApplication.class, args);
+        log.info("Logging with Lombok.");
     }
 
 }
