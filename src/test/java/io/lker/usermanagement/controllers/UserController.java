89671d64108dbeb45de4a6eb46ee23763c5b13d3
Ryan <ryantwalk@gmail.com>
2019-01-06 12:54:18 -0500

    user controller. Closes #9.

---
+++ src/test/java/io/lker/usermanagement/controllers/UserController.java
@@ -0,0 +1,35 @@
+package io.lker.usermanagement.controllers;
+
+import io.lker.usermanagement.model.User;
+import io.lker.usermanagement.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Disallow field of ID to be altered
+    // via forms/requests
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @GetMapping("/users/findAll")
+    public Set<User> findAllUsers(){
+        return userService.findAll();
+    }
+
+}
