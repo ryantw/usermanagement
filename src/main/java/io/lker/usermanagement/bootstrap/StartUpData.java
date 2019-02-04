a7a0c526906745100803a0dc511352340f8f8e73
Ryan Walker <ryantwalk@gmail.com>
2019-02-04 13:48:01 -0500

    add test users and allow cors from local machines

--- src/main/java/io/lker/usermanagement/bootstrap/StartUpData.java
+++ src/main/java/io/lker/usermanagement/bootstrap/StartUpData.java
@@ -1,5 +1,6 @@
 package io.lker.usermanagement.bootstrap;
 
+import io.lker.usermanagement.model.Roles;
 import io.lker.usermanagement.model.User;
 import io.lker.usermanagement.services.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -21,14 +22,28 @@ public class StartUpData implements CommandLineRunner {
 
     private void loadData(){
         User user1 = User.builder().id(1L).firstName("Ryan").lastName("Walker")
-                .emailAddress("r@r.com").build();
+                .emailAddress("ryanwalker@example.com")
+                .roles(Roles.ADMIN).build();
         User user2 = User.builder().id(2L).firstName("Alicia").lastName("Walker")
-                .emailAddress("a@a.com").build();
-        User user3 = User.builder().id(3L).firstName("John").lastName("Smith")
-                .emailAddress("js@js.com").build();
+                .emailAddress("aliciawalker@example.com").build();
+        User user3 = User.builder().id(3L).firstName("Landon").lastName("Gavin")
+                .emailAddress("landongavin@example.com").build();
+        User user4 = User.builder().id(3L).firstName("Nick").lastName("Schlenk")
+                .emailAddress("nickschlenk@example.com").build();
+        User user5 = User.builder().id(3L).firstName("Luke").lastName("Walker")
+                .emailAddress("lukewalker@example.com").build();
+        User user6 = User.builder().id(3L).firstName("Barbara").lastName("Gavin")
+                .emailAddress("barbaragavin@example.com").build();
+        User user7 = User.builder().id(3L).firstName("Vince").lastName("Stratful")
+                .emailAddress("vstrat@gmail.com").build();
 
         userService.save(user1);
         userService.save(user2);
         userService.save(user3);
+        userService.save(user4);
+        userService.save(user5);
+        userService.save(user6);
+        userService.save(user7);
+
     }
 }
--- src/main/java/io/lker/usermanagement/controllers/UserController.java
+++ src/main/java/io/lker/usermanagement/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RequestMapping("/api/admin/users")
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 public class UserController {
 
