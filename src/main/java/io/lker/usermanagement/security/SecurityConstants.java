19b3aaeae919044f260b3963ea909643b40df863
Ryan Walker <ryantwalk@gmail.com>
2019-02-10 18:48:39 -0500

    add a filter to intercept authentication requests and assign a JWT if succeeds. #2.

---
+++ src/main/java/io/lker/usermanagement/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package io.lker.usermanagement.security;
+
+public class SecurityConstants {
+    public static final String SECRET = "AardvarksAndJWTs";
+    public static final long EXPIRATION_DATE = 864_000_000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users/sign-up";
+}
