1774205b016686ef6aaf65b202e78c2af37dedd2
Ryan Walker <ryantwalk@gmail.com>
2019-02-10 18:25:36 -0500

    add bean to get autowired.

--- src/main/java/io/lker/usermanagement/UsermanagementApplication.java
+++ src/main/java/io/lker/usermanagement/UsermanagementApplication.java
@@ -3,10 +3,17 @@ package io.lker.usermanagement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @Slf4j
 public class UsermanagementApplication {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UsermanagementApplication.class, args);
