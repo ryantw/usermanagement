9df3ad91d1a1c7114a47d947c0a31334b83a9425
Ryan Walker <ryantwalk@gmail.com>
2019-02-12 18:19:43 -0500

    add in some test data w/ new bcrypt pw

--- src/main/java/io/lker/usermanagement/bootstrap/StartUpData.java
+++ src/main/java/io/lker/usermanagement/bootstrap/StartUpData.java
@@ -4,15 +4,18 @@ import io.lker.usermanagement.model.Roles;
 import io.lker.usermanagement.model.User;
 import io.lker.usermanagement.services.UserService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartUpData implements CommandLineRunner {
 
     private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public StartUpData(UserService userService) {
+    public StartUpData(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -23,11 +26,13 @@ public class StartUpData implements CommandLineRunner {
     private void loadData(){
         User user1 = User.builder().id(1L).firstName("Ryan").lastName("Walker")
                 .emailAddress("ryanwalker@example.com")
+                .password(bCryptPasswordEncoder.encode("test"))
                 .roles(Roles.ADMIN).build();
         User user2 = User.builder().id(2L).firstName("Alicia").lastName("Walker")
                 .emailAddress("aliciawalker@example.com").build();
         User user3 = User.builder().id(3L).firstName("Landon").lastName("Gavin")
-                .emailAddress("landongavin@example.com").build();
+                .emailAddress("landongavin@example.com")
+                .password(bCryptPasswordEncoder.encode("test")).build();
         User user4 = User.builder().id(4L).firstName("Nick").lastName("Schlenk")
                 .emailAddress("nickschlenk@example.com").build();
         User user5 = User.builder().id(5L).firstName("Luke").lastName("Walker")
