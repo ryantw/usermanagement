48af0d8f040479782a4575fbd39cd79c6c4ade2d
Ryan Walker <ryantwalk@gmail.com>
2019-02-12 19:18:42 -0500

    returned simple json for 404 message

--- src/main/java/io/lker/usermanagement/controllers/advice/UserNotFoundAdvice.java
+++ src/main/java/io/lker/usermanagement/controllers/advice/UserNotFoundAdvice.java
@@ -1,6 +1,7 @@
 package io.lker.usermanagement.controllers.advice;
 
 import io.lker.usermanagement.util.exceptions.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class UserNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String userNotFoundHandler(UserNotFoundException ex){
-        return ex.getMessage();
+    public String userNotFoundHandler(UserNotFoundException ex) {
+        log.error(ex.getMessage());
+        return "{ \"error\": \"Could not find user.\" }";
     }
 }
