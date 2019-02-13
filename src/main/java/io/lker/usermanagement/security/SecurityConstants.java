cace277822da1a951a031b4f7c6badc1cc41cf73
Ryan Walker <ryantwalk@gmail.com>
2019-02-12 19:20:32 -0500

    set expiration date to 15m

--- src/main/java/io/lker/usermanagement/security/SecurityConstants.java
+++ src/main/java/io/lker/usermanagement/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package io.lker.usermanagement.security;
 
 public class SecurityConstants {
     public static final String SECRET = "AardvarksAndJWTs";
-    public static final long EXPIRATION_DATE = 864_000_000;
+    public static final long EXPIRATION_DATE = 900_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/users/sign-up";
